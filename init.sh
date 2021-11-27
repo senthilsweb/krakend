60e25caa7a679d54eeea4df448dceb8410f154a0
senthilsweb <senthilsweb@gmail.com>
2021-11-26 19:02:00 -0500

    Modified config file

--- init.sh
+++ init.sh
@@ -1,2 +1,1 @@
-echo $REST_ENDPOINT
-sed -i -r 's/^(const REST_ENDPOINT =).*/\1 $REST_ENDPOINT/' ./app.config.js
+sed -i -r 's/^(const REST_ENDPOINT =).*/\1 $REST_ENDPOINT/' /etc/krakend/app.config.js
